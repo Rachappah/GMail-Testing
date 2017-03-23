a9d0e4f4135a72d135fcaacf38183b159e30c6ef
Rachappah <Rachappa@HP>
2017-03-23 19:37:50 +0530

    updated

--- glogin/book.py
+++ glogin/book.py
@@ -8,6 +8,5 @@ from glogin.signin import driver
 if __name__ == '__main__':
     driver=webdriver.Chrome()
     driver.get("//http:www.gmail.com")
-    driver.switch_to_alert()
     driver.close()
     
